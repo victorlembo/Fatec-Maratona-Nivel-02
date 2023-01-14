c0fbcaa7d2f2e2301de145aad4b944b1954489f9
Victor Hugo <victordovalle88@gmail.com>
2023-01-14 10:48:28 -0300

    1174 1176 1181

---
+++ 1174.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    double a[100];
+    int i;
+
+    for (i = 0; i < 100; i++)
+    {
+        scanf("%lf", &a[i]);
+    }
+    for (i = 0; i < 100; i++)
+    {
+        if (a[i] <= 10)
+        {
+            printf("A[%d] = %.1lf\n", i, a[i]);
+        }
+    }
+    return 0;
+}
